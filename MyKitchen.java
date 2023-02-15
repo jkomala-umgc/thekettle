93d1c5c9e012c290af2ff0e12f7c521bcf1f372c
bottledship <76917080+bottledship@users.noreply.github.com>
2023-02-15 19:06:22 +0700

    Edited code to comply with the submissions requirement

--- MyKitchen.java
+++ MyKitchen.java
@@ -3,6 +3,7 @@ public class MyKitchen {
     public static void main(String[] args) {
         Scanner interrogator = new Scanner(System.in);
         Kettle myKettle = new Kettle();
+        System.out.println("The Kettle by James Komala \n CMIS 242/6981 \n 15 February 2023");
 
         System.out.print("Enter the volume of water you are boiling (ml): ");
         int waterLevel = interrogator.nextInt();
